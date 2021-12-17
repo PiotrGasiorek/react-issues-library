b01dd5737f6404cfc4e370e3b37978aeaa9e102f
Piotr <piotrjgasiorek@gmail.com>
2021-12-17 04:33:20 +0100

    Added classes to main header and got rid of todos

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,29 +1,13 @@
+import { useSelector } from 'react-redux';
 import PostCardList from '../components/PostCardList';
 
-function Home({ posts }) {
+function Home() {
+  const { posts } = useSelector(state => state);
   return (
-    <div>
-      <h1>Feel free to checkout some of our posts with react issues</h1>
-      <ul>
-        <li>1. Załaduj oraz wyświetl ostatnie 10 issues.</li>
-        <li>2. Zaimplementuj paginację po 5 elementów - infinite scroll.</li>
-        <li>Done 3. Każdy "kafelek" issue powinien zawierać numer issue, tytuł oraz datę utworzenia.</li>
-        <li>3. Kiedy klikniemy w wybrany issue, powinno nas przekierować na stronę z zawartością - stan issue, tytuł, treść issue.</li>
-        <li>4. Pod wybranym issue, dodaj opcję oceniania issue (tak jak na StackOverflow, inkrementowanie/dekremntowanie punktów) jeden użytkownik może w nieskończoną ilość zaplusować bądź też zminusować issue.</li>
-        <li>5. Zakres wyświetlania oceny jest następująca:
-          - poniżej -99 wyświetla "99!!"
-          - od -99 do 99 wyświetla wybraną liczbę
-          - powyżej 99 wyświetla "99!!" </li>
-        <li>6. Ilość ocen ma być widoczna po zamknięciu strony, po ponownym otwarciu stan ocen ma wskazywać na taki sam.</li>
-
-        <li>Opcjonalne do wykonania:</li>
-        <li>1. Lokalna wyszukiwarka issue</li>
-        <li>2. Animacje oraz przejścia</li>
-
-      </ul>
-      <p>https://github.com/facebook/react/issues</p>
-      <PostCardList posts={posts}/>
-    </div>
+    <>
+      <h1 className='hp__title fade-in-left'>Feel free to checkout some of our posts with react issues from official Github API</h1>
+      <PostCardList postsData={posts}/>
+    </>
   );
 }
 
