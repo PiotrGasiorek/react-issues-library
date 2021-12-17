d92aac699d4b9626f88fff42102d9421332d004b
Piotr <piotrjgasiorek@gmail.com>
2021-12-17 04:35:43 +0100

    Added rating btns and animation

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import RatingButtons from "./RatingButtons";
 
-function PostCard({ title, id, date }) {
+function PostCard({ title, id, date, rating }) {
   const postPageUrl = `post/${id}`;
   return (
-    <div className="card">
-      <h3 className="card__title">{title}</h3>
-      <p className="card__id">{id}</p>
+    <div className="card fade-in-bottom">
       <time className="card__date">{date}</time>
+      <h3 className="card__title">{title}</h3>
+      {/* <p className="card__id">{id}</p> */}
+      <RatingButtons rating={rating}/>
       <Link className="card__link" to={postPageUrl}>Show issue</Link>
     </div>
   );
