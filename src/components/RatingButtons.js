c2ac5c84a7110ee390ecfaf17c64381bb9e6f4cf
Piotr <piotrjgasiorek@gmail.com>
2021-12-17 04:37:08 +0100

    Created component to manage rating

---
+++ src/components/RatingButtons.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import FeatherIcon from 'feather-icons-react';
+
+function RatingButtons({ rating }) {
+  const [postRating, setPostRating] = useState(rating);
+  const increaseRating = () => {
+    setPostRating(postRating + 1);
+  }
+  const decreaseRating = () => {
+    setPostRating(postRating - 1);
+  }
+  return (
+    <div className="rating">
+      <button className="rating__btn rating__btn--increase" onClick={increaseRating}>
+        <FeatherIcon className="rating__btn__icon" icon="thumbs-up" />
+      </button>
+      <span className="rating__score">{
+        postRating > 99 ?
+          '+99!!' : 
+          postRating < -99 ? 
+            '-99!!' : postRating
+      }</span>
+      <button className="rating__btn rating__btn--decrease" onClick={decreaseRating}>
+        <FeatherIcon className="rating__btn__icon" icon="thumbs-down" />
+      </button>
+    </div>
+  );
+}
+
+export default RatingButtons;
