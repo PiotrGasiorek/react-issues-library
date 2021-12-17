9ac4034c041cb2d2d7087194d71bb028f40cef17
Piotr <piotrjgasiorek@gmail.com>
2021-12-17 04:31:14 +0100

    Added active class for link in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,11 @@ import logo from '../logo.svg';
 
 function Nav() {
   return (
-    <nav className="nav">
+    <nav className="nav fade-in-top">
       <NavLink to="/" className="nav__logo"><img src={logo} alt="Our logo"/></NavLink>
-      <NavLink to="/" className="nav__link">Home</NavLink>
+      <NavLink to="/" className={({isActive}) =>
+        "nav__link" + (isActive ? " nav__link--active" : "")
+      }>Home</NavLink>
     </nav>
   );
 }
